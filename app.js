bc12bf40b61f3bb3ab8754299b8a81091b036406
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-12-23 12:26:00 +0300

    js

---
+++ app.js
@@ -0,0 +1,15 @@
+const section = document.querySelector('section'),
+  overlay = document.querySelector('.overlay'),
+  showBtn = document.querySelector('.show-modal'),
+  closeBtn = document.querySelector('.close-btn');
+
+showBtn.addEventListener('click', () => {
+  section.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  section.classList.remove('active');
+});
+overlay.addEventListener('click', () => {
+  section.classList.remove('active');
+});
